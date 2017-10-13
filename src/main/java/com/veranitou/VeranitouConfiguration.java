13dbd40d38761e11a36f5665ffc1da590c76bd39
root <nicolas.ard@gmail.com>
2017-10-12 21:29:16 -0400

    First commit of something a little functional using Spring-Security

---
+++ src/main/java/com/veranitou/VeranitouConfiguration.java
@@ -0,0 +1,41 @@
+package com.veranitou;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.apache.log4j.Logger;
+
+@Configuration
+@EnableWebSecurity
+public class VeranitouConfiguration
+   extends WebSecurityConfigurerAdapter {
+
+   	final static Logger logger = Logger.getLogger(VeranitouConfiguration.class);
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) {
+  	try{
+  		auth.inMemoryAuthentication().withUser("user").password("123").roles("USER");
+  	}
+	catch (Exception ex){
+		logger.error("Excepcion loading VeranitouConfiguration",ex);
+	}
+  }
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+	    http.authorizeRequests()
+		.antMatchers("/hola/**").access("hasRole('ROLE_USER')")
+		.and()
+		    .formLogin().loginPage("/login").failureUrl("/login?error")
+		    .usernameParameter("user").passwordParameter("123")
+		.and()
+		    .logout().logoutSuccessUrl("/login?logout")
+		.and()
+		    .csrf();
+	}
+}
---
+++ src/main/java/com/veranitou/controllers/UsuarioController.java
@@ -0,0 +1,58 @@
+package com.veranitou.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.List;
+import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.apache.log4j.Logger;
+import com.veranitou.dao.configuration;
+
+import org.hibernate.Session;
+import com.veranitou.dao.HibernateUtil;
+import com.veranitou.dao.Consulta;
+
+@Controller
+public class UsuarioController {
+    
+    final static Logger logger = Logger.getLogger(UsuarioController.class);
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login(
+        @RequestParam(value = "error", required = false) String error,
+        @RequestParam(value = "logout", required = false) String logout) {
+
+        ModelAndView model = new ModelAndView();
+
+        model.addObject("appVersion", "asd");
+
+        if (error != null) {
+            model.addObject("error", "Invalid username and password!");
+        }
+
+        if (logout != null) {
+            model.addObject("msg", "You've been logged out successfully.");
+        }
+        model.setViewName("login");
+
+        return model;
+
+    }
+
+    @RequestMapping(value = "/admin**", method = RequestMethod.GET)
+    public ModelAndView adminPage() {
+
+        ModelAndView model = new ModelAndView();
+        model.addObject("title", "Spring Security Custom Login Form");
+        model.addObject("message", "This is protected page!");
+        model.setViewName("admin");
+
+        return model;
+
+    }
+
+}

dab7d462d3e40b8bec8f934874eba9a1f69d103f
root <nicolas.ard@gmail.com>
2017-10-18 21:28:38 -0400

    #3 first functional use of the rest service

--- src/main/resources/static/home.js
+++ src/main/resources/static/home.js
@@ -1,12 +1,16 @@
 
 angular.module('VeranitouConsultasApp', []).controller('VeranitouConsultasController', function($scope,$http) {
    $scope.ListaConsultasUrl = "/secure/consultas/lista/";
-   $scope.greeting = "Hello World";
-   $scope.consultas = ["Banana", "Orange", "Apple", "Mango"];
+   $scope.qty = 2;
+   $scope.page = 1;
+   $scope.consultas = [];
 
-   $scope.loadConsultas = function(){
+   $scope.loadConsultas = function(qty,page){
     console.log("Loading Consultas...")
-    $http.get($scope.ListaConsultasUrl).then(function(response) {
+    $scope.qty = qty;
+    $scope.page = page;
+    gData = {params:{qty:$scope.qty,page:$scope.page}};
+    $http.get($scope.ListaConsultasUrl,gData).then(function(response) {
             console.log(response.data);
             $scope.consultas = response.data;
         });

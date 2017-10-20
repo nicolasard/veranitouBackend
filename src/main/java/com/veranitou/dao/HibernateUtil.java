6824ddb3b990f4dc920f9c28577b17d7f81b6122
root <nicolas.ard@gmail.com>
2017-10-20 19:27:56 -0400

    Fixed the absolute path config for hibernate. Now the files are taken from the resources path

--- src/main/java/com/veranitou/dao/HibernateUtil.java
+++ src/main/java/com/veranitou/dao/HibernateUtil.java
@@ -2,15 +2,25 @@ package com.veranitou.dao;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.apache.log4j.Logger;
+import java.net.URL;
 
 public class HibernateUtil {
 
+    private static final Logger logger = Logger.getLogger(HibernateUtil.class);
     private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
         try {
+            logger.info("Building session factory...");
+
+            // Get the config path from ./resources/
+            ClassLoader classLoader = HibernateUtil.class.getClassLoader();
+            URL resource = classLoader.getResource("hibernate.cfg.xml");
+            logger.info("hibernate.cfg.xml location: "+resource.getPath());
+            
             // Create the SessionFactory from hibernate.cfg.xml
-            return new Configuration().configure("file:///home/nardison/veranitouBackend/hibernate.cfg.xml").buildSessionFactory();
+            return new Configuration().configure(resource).buildSessionFactory();
         }
         catch (Throwable ex) {
             // Make sure you log the exception, as it might be swallowed

5ac36c4f60c19633c4ad6a38fcd1ff0255b94241
root <nicolas.ard@gmail.com>
2017-10-28 14:44:21 -0400

    Minor dev. in frontend

--- src/main/resources/static/home.js
+++ src/main/resources/static/home.js
@@ -1,11 +1,15 @@
 
 angular.module('VeranitouConsultasApp', []).controller('VeranitouConsultasController', function($scope,$http) {
    $scope.ListaConsultasUrl = "/secure/consultas/lista/";
+   $scope.CantidadRegistrosUrl = "/secure/consultas/cantidad/"
+   $scope.CantidadRegistros = 0;
+   $scope.CatidadPaginas = 0;
    $scope.qty = 2;
    $scope.page = 1;
    $scope.consultas = [];
 
    $scope.loadConsultas = function(qty,page){
+    $scope.loadCantidad();
     console.log("Loading Consultas...")
     $scope.qty = qty;
     $scope.page = page;
@@ -14,6 +18,26 @@ angular.module('VeranitouConsultasApp', []).controller('VeranitouConsultasContro
             console.log(response.data);
             $scope.consultas = response.data;
         });
+
+    //Calculo la cantidad de hojas 
+    var cantidad = $scope.CantidadRegistros / $scope.qty;
+    $scope.CatidadPaginas = cantidad;
    };
 
+   $scope.loadCantidad = function(){
+    console.log("Cargando Cantidad de registros...");
+
+    $http.get($scope.CantidadRegistrosUrl).then( function(response){
+            console.log(response.data);
+            $scope.CantidadRegistros = response.data;
+   });
+   };
+
+   $scope.paginaSiguiente = function(){
+    
+   };
+
+   $scope.paginaAnterior = function(){
+
+   };
 });
